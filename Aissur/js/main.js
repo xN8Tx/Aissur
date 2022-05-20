03d7cee6ce1cec67c6b8f1899c96a4e0a4f742b8
Евгений Нейт <61351123+xN8Tx@users.noreply.github.com>
2022-05-20 13:05:07 +0700

    Delete Aissur directory

--- Aissur/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function functionName() {
-  $('.header__burger').click(function(){
-        $('.header__burger, .header__menu').toggleClass('active');
-	$('body').toggleClass('lock');
-});
-})
